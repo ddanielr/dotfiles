c4ccc1a3cc561cb77c11f6145e87a6766d7839e5
Daniel Roberts <ddrobe3@evoforge.org>
2019-10-10 12:21:44 -0400

    Added Sudo code for file install

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Run this script to install the dotfiles
+
+# Check and see what files are in the present directory
+# Check and see if those files exist at ~ and if so, make .old copies
+# copy in the new files
